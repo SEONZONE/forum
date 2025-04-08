4479adfe17271a7b95224e2fc2dda6260fa6acf3
seonzone <tjdwhd2247@naver.com>
2025-04-08 23:23:23 +0900

    feat: revalidate 설정 추가

--- app/list/page.js
+++ app/list/page.js
@@ -2,6 +2,8 @@ import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 60;
+
 export default async function List() {
   const db = (await connectDB).db("forum");
   const result = await db.collection("post").find().toArray();
